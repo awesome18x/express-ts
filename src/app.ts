36323e62074461cb184c7b435e29280e6eff1e67
duynv1895 <duynv@ql6625.fr>
2020-09-21 15:36:27 +0700

    Cau hinh project

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,40 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const app = express();
 
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('<h1>Server building by express and typescript</h1>');
-})
+class App {
+    public app: express.Application;
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000.');
-})
+    public constructor() {
+        this.app = express();
+        this.middlewares();
+        this.database();
+        this.routes();
+    }
+
+    private middlewares(): void {
+        this.app.use(express.json());
+        this.app.use(cors());
+    }
+
+    private database(): void {
+        mongoose.connect('mongodb://localhost:27017/express-typescript', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(
+            () => console.log('Ket noi database thanh cong'),
+            (error) => console.log('Ket noi database loi')
+        );
+    }
+
+    private routes(): void {
+        this.app.get('/', (req, res) => {
+            return res.send('<h1>Nguyen Van Duy - duynv1895@gmail.com</h1>')
+        })
+    }
+}
+
+export default new App().app
